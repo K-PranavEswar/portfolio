95404f1e8b0dc0ff5799ce87907afbf6b20e4c94
Pranav Eswar <pranav1495.pranavartist1@gmail.com>
2026-04-14 21:21:04 +0530

    remove nested router fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
