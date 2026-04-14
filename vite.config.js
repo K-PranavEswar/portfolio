65844147ac9915a6c216e64867300bfddcc9a083
Pranav Eswar <pranav1495.pranavartist1@gmail.com>
2026-04-14 21:08:14 +0530

    portfolio production ready

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    chunkSizeWarningLimit: 1500,
     cssCodeSplit: true,
     rollupOptions: {
       output: {
@@ -19,7 +19,6 @@ export default defineConfig({
           if (id.includes('react-router-dom')) {
             return 'router'
           }
-          return undefined
         },
       },
     },
